982b1718094abbb661032e1ca93fa7b4855a96bd
Vojdan Bojcev <vbojcev@uwaterloo.ca>
2022-07-01 14:45:38 -0400

    adding comments

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CoDriver_Pi
-The code that runs on CoDriver's microcontroller.
+The code that runs on CoDriver's microcontroller. CoDriver uses Python and Adafruit's circuitpython libraries to interface with sensors.
+
+component_tests contains python scripts to test the IMU/SWS, GPS, etc.
--- component_tests/imu.py
+++ component_tests/imu.py
@@ -2,16 +2,34 @@ import time
 import board
 import adafruit_mpu6050
 
-X_A_OFFSET = 0.514183
-Y_A_OFFSET = 0.287291
-Z_A_OFFSET = -1.59902
 
-i2c = board.I2C()
-mpu = adafruit_mpu6050.MPU6050(i2c)
+#calculate offsets for IMU1, for calibration
+
+if 1:
+	#IMU1 accel. calibration
+	X_A_OFFSET = 0.514183
+	Y_A_OFFSET = 0.287291
+	Z_A_OFFSET = -1.59902
+	#IMU1 gyro calibration(TODO)
+	X_G_OFFSET = 0
+	Y_G_OFFSET = 0
+	Z_G_OFFSET = 0
+else:
+	#IMU2 accel. calibration (TODO)
+	X_A_OFFSET = 0
+	Y_A_OFFSET = 0
+	Z_A_OFFSET = 0
+	#IMU2 gyro calibration (TODO)
+	X_G_OFFSET = 0
+	Y_G_OFFSET = 0
+	Z_G_OFFSET = 0
+
+i2c = board.I2C()	#initialize the i2c interface
+imu = adafruit_mpu6050.MPU6050(i2c)	#initialize the mpu object
 
 while True:
-	print(mpu.acceleration[0]-X_A_OFFSET,",",mpu.acceleration[1]-Y_A_OFFSET,",",mpu.acceleration[2]-Z_A_OFFSET)
-	#print("%.5f,%.5f,%.5f" % (mpu.gyro))
-	#print("Temperature: %.2f C" % mpu.temperature)
-	#print("")
-	time.sleep(0.01)
+	#calibration was done by printing in comma delineation, finding average offest in Excel
+	print(imu.acceleration[0]-X_A_OFFSET,",",imu.acceleration[1]-Y_A_OFFSET,",",imu.acceleration[2]-Z_A_OFFSET)
+	#print("%.5f,%.5f,%.5f" % (imu.gyro))	#test gyroscope
+	#print("Temperature: %.2f C" % mpu.temperature)	#test temperature
+	time.sleep(0.01)	#print every 10ms
