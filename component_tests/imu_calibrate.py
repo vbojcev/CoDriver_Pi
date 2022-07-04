81f53649122d68286a7593bcc61d3d7cb6d9392a
Vojdan Bojcev <vbojcev@uwaterloo.ca>
2022-07-04 14:20:28 -0400

    Create imu_calibrate.py

---
+++ component_tests/imu_calibrate.py
@@ -0,0 +1,30 @@
+import time
+import board
+import adafruit_mpu6050
+import csv
+
+#7/4/2022: CREATED; NOT YET VERIFIED.
+
+#calculate offsets for IMU1, for calibration
+#NOTE: Assumes z axis is perfectly vertical and subtracts the acceleration due to gravity, g=9.80665 m/s^2
+
+G = 9.80665
+
+x_a_offset = 0
+y_a_offset = 0
+z_a_offset = 0
+
+i2c = board.I2C()	#initialize the i2c interface
+imu = adafruit_mpu6050.MPU6050(i2c)	#initialize the mpu object
+
+for i in range(10000):  #100 seconds calibration run.
+
+    x_a_offset = x_a_offset + ((i*x_a_offset)-imu.acceleration[0])/(i+1) # Coninually-updated average formula
+    y_a_offset = y_a_offset + ((i*y_a_offset)-imu.acceleration[1])/(i+1)
+    z_a_offset = z_a_offset + ((i*z_a_offset)-imu.acceleration[2])/(i+1)
+
+	#print(imu.acceleration[0],",",imu.acceleration[1],",",imu.acceleration[2])
+	#print(imu.gyro[0],",",imu.gyro[1],",",imu.gyro[2])
+    time.sleep(0.01)
+
+print("Final offset values:\tX=",x_a_offset,"\tY=",y_a_offset,"\tZ=",z_a_offset-G)
