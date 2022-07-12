1806587ff1dc3c3717b48b0e4f4c553eb71b634d
Vojdan Bojcev <vbojcev@uwaterloo.ca>
2022-07-11 20:37:45 -0400

    Update imu_calibrate.py

--- component_tests/imu_calibrate.py
+++ component_tests/imu_calibrate.py
@@ -10,6 +10,14 @@ import csv
 
 G = 9.80665 #m/s^2
 
+IMU_x_adj = 0.3992678427551265
+IMU_y_adj = 0.10196126755065921
+IMU_z_adj = -0.23886650688167066
+
+SWS_x_adj = 0.49935046006978273
+SWS_y_adj = 0.22327307146911718
+SWS_z_adj = -1.6066040844471896
+
 x_a_offset = 0
 y_a_offset = 0
 z_a_offset = 0
@@ -23,6 +31,10 @@ print("Starting IMU Calibration...")
 for i in range(12000):  #2-minute calibration run.
 
     a_x,a_y,a_z=imu.acceleration
+
+    a_x= a_x - IMU_x_adj
+    a_y= a_y - IMU_y_adj
+    a_z= a_z - IMU_z_adj
 
     x_a_offset = x_a_offset + (a_x-x_a_offset)/(i+1) # Coninually-updated average formula
     y_a_offset = y_a_offset + (a_y-y_a_offset)/(i+1)
@@ -44,6 +56,10 @@ print("Starting SWS Calibration")
 for i in range(12000):  #2-minute calibration run.
 
     a_x,a_y,a_z=sws.acceleration
+
+    a_x= a_x - SWS_x_adj
+    a_y= a_y - SWS_y_adj
+    a_z= a_z - SWS_z_adj
 
     x_a_offset = x_a_offset + (a_x-x_a_offset)/(i+1) # Coninually-updated average formula
     y_a_offset = y_a_offset + (a_y-y_a_offset)/(i+1)
