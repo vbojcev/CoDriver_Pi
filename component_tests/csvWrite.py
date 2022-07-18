374604ed86499b2ef67dc218a469f8c19af4084d
Vojdan Bojcev <vbojcev@uwaterloo.ca>
2022-07-18 13:00:55 -0400

    Update csvWrite.py

--- component_tests/csvWrite.py
+++ component_tests/csvWrite.py
@@ -11,25 +11,30 @@ import csv
 class DataManager:
 
     def __init__(self):
-        self.content=[0]
+        self.content=[0,0]
         self.numRows = 0
         self.fileIndex = 0
-        self.currentFile = open('data0.csv', 'w', newline='')
+        self.currentFile = open('data0.csv', 'w')
         self.writer = csv.writer(self.currentFile)
 
     def record(self):   #UNTESTED
+        self.numRows += 1
         self.updateTime(time.time())
         self.writer.writerow(self.content)
-        self.numRows += 1
         if (self.numRows > 1000):   #Maximum file size reached
             self.currentFile.close()
             self.fileIndex += 1
-            self.currentFile = open('data'+str(self.fileIndex)+'.csv', 'w', newLine='')
+            print('Creating new file... name: '+'data'+str(self.fileIndex)+'.csv')
+            self.currentFile = open('data'+str(self.fileIndex)+'.csv', 'w')
             self.writer = csv.writer(self.currentFile)
             self.numRows = 0
 
     def updateTime(self, timeStamp):
         self.content[0] = timeStamp
+        self.content[1] = self.numRows
+
+    def cleanUp(self):
+        self.currentFile.close()
 
 #/////////////////////////////////////////////////////////
 
@@ -37,5 +42,9 @@ class DataManager:
 #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 if __name__ == '__main__':
     #Main Loop
+    data = DataManager()
+    for i in range(3500):
+        data.record()
+        time.sleep(0.01)
     pass
 #/////////////////////////////////////////////////////////
